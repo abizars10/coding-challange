df82c46d57253d08899090014d92559b7c94d8b9
Abizar <abizararrifai@gmail.com>
2024-02-07 04:10:23 +0700

    seventh challenge

---
+++ Documents/coding-challange/evenOrOdd.js
@@ -0,0 +1,14 @@
+// function evenOrOdd(number) {
+//   if(number % 2 === 0){
+//     return 'Even'
+//   }else{
+//     return 'Odd'
+//   }
+
+// Ternari
+//   return (number % 2 === 0) ? 'Even': 'Odd';
+// }
+
+const evenOrOdd = (n) => (n % 2 === 0 ? "Even" : "Odd");
+
+console.log(evenOrOdd(210));
